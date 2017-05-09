aeb2abed01a3d8380a2082de8cc7784c25b099a9
david gray <davidseangray@gmail.com>
2017-05-09 14:24:55 +0100

    adds 1 to temrature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -10,4 +10,9 @@ describe('Thermostat', function(){
   it('should start at 20 degrees', function() {
     expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
+
+  it('turns up the temperature by 1', function() {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,8 +1,13 @@
 'use strict';
 
 function Thermostat() {
-  Thermostat.prototype.temperature = 20
+  this.temperature = 20
 }
+
 Thermostat.prototype.getCurrentTemperature = function() {
   return this.temperature;
+};
+
+Thermostat.prototype.up = function() {
+  this.temperature += 1;
 };
